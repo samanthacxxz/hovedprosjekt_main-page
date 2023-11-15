3f617ba2f2a736926589bd11d4e6220c9f511e79
Samantha Cruz <144262012+samanthacxxz@users.noreply.github.com>
2023-11-15 16:21:02 +0100

    adding js code for slideshow

---
+++ js/script.js
@@ -0,0 +1,60 @@
+/* for slideshow - recommendations */
+const slideshow = document.querySelector('.slideshow_recs');
+const slides = slideshow.querySelectorAll('.slideshow_slide');
+const controls = slideshow.querySelectorAll('.slideshow_control-button');
+
+let index = 0;
+const totalSlides = slides.length;
+const lastIndex = slides.length - 1;
+
+const goToPreviousSlide = () => {
+	if (index === 0) {
+		index = lastIndex;
+	} else {
+		index = index - 1;
+	}
+
+	console.log(index);
+
+	// update classes
+	slides.forEach(slide => {
+		slide.classList.remove('slideshow_slide--visible');
+	});
+
+	slides[index].classList.add('slideshow_slide--visible');
+};
+
+const goToNextSlide = () => {
+	// increase index
+	if (index < lastIndex) {
+		index = index + 1;
+	} else {
+		index = 0;
+	}
+
+	console.log(index);
+
+	// update classes
+	slides.forEach(slide => {
+		slide.classList.remove('slideshow_slide--visible');
+	});
+
+	slides[index].classList.add('slideshow_slide--visible');
+};
+
+
+const changeSlide = (event) => {
+	const button = event.currentTarget;
+
+	if (button.dataset.direction === 'previous') {
+		goToPreviousSlide();
+	}
+
+	if (button.dataset.direction === 'next') {
+		goToNextSlide();
+	}
+}
+
+controls.forEach(button => {
+	button.addEventListener('click', changeSlide);
+})
